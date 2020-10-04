7067cc4e0f8dc42d5bf124d73883b137a0e359d0
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-04 14:59:16 -0700

    add renderCourses

--- js-frontend/src/appContainer.js
+++ js-frontend/src/appContainer.js
@@ -22,11 +22,24 @@ class appContainer {
       .then((resp) => resp.json())
       .then((data) => {
         data.forEach((course) => {
-          new Course(course.name, course.city, course.state);
+          new Course(course.name, course.city, course.state, course.comment);
         });
+        this.renderCourses();
       })
       // fetch courses
       // render
       .catch((error) => alert(error));
+  }
+
+  renderCourses() {
+    const ul = document.createElement("UL");
+    const courseDiv = document.getElementById("courseDiv");
+    appContainer.courses.forEach((course) => {
+      const li = document.createElement("LI");
+      li.innerText = course.name;
+      ul.appendChild(li);
+      courseDiv.appendChild(ul);
+    });
+    // document.courseDiv.appendChild(ul);
   }
 }
