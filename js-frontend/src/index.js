26c9cdd6118be02ac6656323f8f4736dd592fcb5
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-04 08:54:51 -0700

    stub out basic structure btn form div

--- js-frontend/src/index.js
+++ js-frontend/src/index.js
@@ -1,7 +1,10 @@
 const BACKEND_URL = "http://localhost:3000";
 
 const app = new appContainer();
-console.log(app.getCourses());
+app.getCourses();
+// instantiates our app
+
+//
 // fetch(`${BACKEND_URL}/courses`)
 //   .then((resp) => resp.json())
 //   .then((resp) => console.log(resp));
