fe76610c1cfcdc5f92972962f77b28014e71401b
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-03 19:00:58 -0700

    create Comment resource

---
+++ js-backend/db/migrate/20201004020044_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.belongs_to :course, null: false, foreign_key: true
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
