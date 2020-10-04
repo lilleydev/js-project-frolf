c31bb40c254e657b3117e75a55fba200917ef069
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-03 20:25:45 -0700

    move fetch to appContainer

--- js-frontend/src/index.js
+++ js-frontend/src/index.js
@@ -1,7 +1,7 @@
 const BACKEND_URL = "http://localhost:3000";
 
 const app = new appContainer();
-console.log(app);
-fetch(`${BACKEND_URL}/courses`)
-  .then((resp) => resp.json())
-  .then((resp) => console.log(resp));
+console.log(app.getCourses());
+// fetch(`${BACKEND_URL}/courses`)
+//   .then((resp) => resp.json())
+//   .then((resp) => console.log(resp));
