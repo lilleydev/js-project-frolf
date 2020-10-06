2d744f2cbec3a17d8492b4f926823457f5bb545e
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-06 09:53:38 -0700

    create action

--- js-backend/app/controllers/courses_controller.rb
+++ js-backend/app/controllers/courses_controller.rb
@@ -5,7 +5,7 @@ class CoursesController < ApplicationController
   end
 
   def create
-    binding.pry
+    # binding.pry
     @course = Course.new(course_params)
     render json: @course if @course.save
   end
