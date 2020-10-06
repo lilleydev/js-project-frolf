bc06e49215c02469f2bae5e37cf0dd3ffafe5eaa
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-06 08:54:42 -0700

    add create route to post

--- js-backend/config/routes.rb
+++ js-backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :comments, only: :index
-  resources :courses, only: %i[index post]
+  resources :courses, only: %i[index create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
