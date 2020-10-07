69c4d653c4b037af83c7cbbdaafa7bb5325a40a0
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-07 11:39:35 -0700

    add destroy route

--- js-backend/config/routes.rb
+++ js-backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :comments, only: :index
-  resources :courses, only: %i[index create]
+  resources :courses, only: %i[index create destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
