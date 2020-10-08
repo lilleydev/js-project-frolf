363e6dd15a6e7d55212c783158bb092cec4a70d9
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-08 08:25:09 -0700

    add getComment func

--- js-frontend/src/comment.js
+++ js-frontend/src/comment.js
@@ -2,4 +2,25 @@ class Comment {
   constructor(content, course) {
     (this.content = content), (this.course = course);
   }
+
+  getComment() {
+    fetch("http://localhost:3000/comments")
+      .then((resp) => resp.json())
+      .then((data) => {
+        data.forEach((course) => {
+          const c = new Course(
+            course.name,
+            course.city,
+            course.state,
+            course.id
+            // course.comment,
+          );
+          this.courses.push(c);
+        });
+        this.renderCourses();
+      })
+      // fetch courses
+      // render
+      .catch((error) => console.error(error));
+  }
 }
