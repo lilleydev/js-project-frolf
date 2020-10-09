bb93a5acef42f02cab860ec114d55396c31bcaa2
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-08 17:01:52 -0700

    delete course from constructor

--- js-frontend/src/comment.js
+++ js-frontend/src/comment.js
@@ -1,21 +1,5 @@
 class Comment {
-  constructor(content, course) {
-    (this.content = content), (this.course = course);
-  }
-
-  getComment() {
-    debugger;
-    fetch("http://localhost:3000/comments")
-      .then((resp) => resp.json())
-      .then((data) => {
-        data.forEach((comment) => {
-          const c = new Comment(this.content, this.course_id);
-          this.courses.push(c);
-        });
-        debugger;
-      })
-      // fetch courses
-      // render
-      .catch((error) => console.error(error));
+  constructor(content) {
+    this.content = content;
   }
 }
