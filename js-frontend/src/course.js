3029de0a4a91ffe9bfb2739c1f7d40949c9db2d3
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-08 17:01:00 -0700

    add comment map to constructor

--- js-frontend/src/course.js
+++ js-frontend/src/course.js
@@ -3,13 +3,18 @@ class Course {
     (this.name = name),
       (this.city = city),
       (this.state = state),
-      (this.id = id);
-      this.comments = comments,
-    // this.comment = comment;
-    // debugger;
-    // app.courses.push(this);
-    // debugger;
+      (this.id = id),
+      (this.comments = comments.map((comment) => new Comment(comment.content)));
+    // this.comments = (comments.map((comment) => new Comment(comment.content)))};
   }
+  //have a comment object *******
+  // to do: display the course with the comments -- toggle button so you can hide comments,
+  // use the button for seeing comments
+
+  // this.comment = comment;
+  // debugger;
+  // app.courses.push(this);
+  // debugger;
   // courseEventListeners() {
   //   const deleteBtn = document.getElementById("deleteButton");
   //   deleteBtn.addEventListener("click", this.delete);
@@ -44,14 +49,15 @@ class Course {
 
   getComment() {
     // debugger;
-    fetch("http://localhost:3000/comments")
+    fetch(`http://localhost:3000/courses/${this.id}/comments`)
       .then((resp) => resp.json())
       .then((data) => {
         console.log(data);
         data.forEach((comm) => {
           debugger;
+          // fetch using nested route
           const c = new Comment(this.content, this.course_id);
-          this.courses.push(c);
+          this.comments.push(c);
         });
         debugger;
       })
