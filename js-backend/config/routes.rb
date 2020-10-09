b13f87d07d122021d336c8696f543824273d15ed
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-08 17:21:10 -0700

    add nested route

--- js-backend/config/routes.rb
+++ js-backend/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments, only: :index
-  resources :courses, only: %i[index create destroy]
+  resources :comments, only: %i[index show]
+  resources :courses, only: %i[index create destroy] do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
