89dc6b6872af9a3ff5fd4e3f4f940f1ee780cec3
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-09 19:15:12 -0700

    add seed data

--- js-backend/db/seeds.rb
+++ js-backend/db/seeds.rb
@@ -3,5 +3,13 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+Course.create(name: 'Kit Carson', city: 'Escondido', state: 'CA')
+Course.create(name: 'Red Mountain', city: 'Mesa', state: 'AZ')
+Course.create(name: 'Brengle Terrance', city: 'Vista', state: 'CA')
+
+Comment.create(content: 'mixed use park', course_id: 1)
+Comment.create(content: '2 separate courses', course_id: 2)
+Comment.create(content: 'bring water!!', course_id: 2)
