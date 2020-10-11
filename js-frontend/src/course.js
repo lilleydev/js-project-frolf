f455f2959198a8b599e2a80df5327a0955ec0bc7
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-10 21:20:37 -0700

    delete commented out code

--- js-frontend/src/course.js
+++ js-frontend/src/course.js
@@ -5,22 +5,13 @@ class Course {
       (this.state = state),
       (this.comments = comments);
     this.id = id;
-    // (this.comments = comments.map(
-    //   (comment) => new Comment(comment.content, comment.course_id)
-    // ));
-    // this.comments = (comments.map((comment) => new Comment(comment.content)))};
   }
-  //have a comment object *******
-  // to do: display the course with the comments -- toggle button so you can hide comments,
-  // use the button for seeing comments
 
   display() {
-    //add new course to page
     const coursesDiv = document.getElementById("coursesDiv");
     const courseDiv = document.createElement("DIV");
 
     const commentForm = document.createElement("FORM");
-    // const commentDiv = document.createElement("DIV");
 
     const h4 = document.createElement("h4");
     const cityP = document.createElement("P");
@@ -31,7 +22,7 @@ class Course {
     const createCommentButton = document.createElement("button");
     const commentInput = document.createElement("INPUT");
     const commentUl = document.createElement("UL");
-    // debugger;
+
     commentUl.id = `${this.id}commentUl`;
     deleteBtn.id = "deleteButton";
     commentButton.id = "commentButton";
@@ -61,13 +52,8 @@ class Course {
     commentForm.appendChild(commentInput);
     commentForm.appendChild(createCommentButton);
 
-    // deleteBtn.addEventListener("click", (e) => this.delete(e));
     commentButton.addEventListener("click", (e) => this.getComment(e));
 
-    // const createCommentButton = document.getElementById("newComment");
-    // createCommentButton.addEventListener("click", (e) => this.addComment(e));
-
-    // console.log(createCommentButton);
     commentForm.addEventListener("submit", (e) => this.addComment(e));
 
     deleteBtn.addEventListener("click", (e) => {
@@ -102,15 +88,8 @@ class Course {
         course_id: this.id,
       }),
     });
-    // .then((resp) => resp.json())
-    // .then((data) => {
-    //   if (data.errors) {
-    //     console.log(info.errors);
-    //   } else {
-    //     app.comments.push(comment);
+
     document.querySelector("input[id=commentInput]").value = "";
-    //   }
-    // });
   }
 
   getComment() {
@@ -120,103 +99,31 @@ class Course {
       .then((data) => {
         console.log(data);
         data.forEach((comm) => {
-          // debugger;
-          // fetch using nested route
           const c = new Comment(comm.content, comm.course_id);
-          // if c.content == undefined
 
-          // else
           app.comments.push(c);
-          // debugger;
         });
-        // debugger;
-        // if this.comments = [] ***
         const length = this.comments.length;
         if (length < 1) {
           alert("There are no comments for this course");
         } else {
           this.renderComment();
         }
-        // this.comments.length > 0
       })
-      // fetch courses
-      // render
       .catch((error) => console.error(error));
   }
 
-  // createComment(comment) {
-  //   fetch("http://localhost:3000/comments", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       comment: {
-  //         content: comment.content,
-  //         course_id: comment.course_id,
-  //       },
-  //     }),
-  //   })
-  //     .then((r) => r.json())
-  //     .then((info) => {
-  //       if (info.errors) {
-  //         console.log(info.errors);
-  //       } else {
-  //         debugger;
-  //         // newPost.reset();
-  //         // const u = info.included[0];
-  //         // renderPost(info.data, u);
-  //         // info.data = [info.data];
-  //         // app.addBlogPosts(info);
-  //       }
-  //     });
-  // }
-  // document.getElementById("commentInput").reset();
-  // document.querySelector("input[id=twitter_handle_input]").value = "";
-  // form.innerText = "";
-
-  // .then((resp) => resp.json())
-  // .then((data) => {
-  //   if (data.errors) {
-  //     console.log(info.errors);
-  //   } else {
-  //     // course.id = data.id;
-
-  //     // debugger;
-  //     // comment.save();
-  //     // this.renderNewCourse(data);
-  //     app.comments.push(comment);
-  //   }
-  // });
-  //   document.getElementById("commentForm").reset();
-  //   // debugger;
-  // }
-
   renderComment() {
-    // debugger;
     this.comments.forEach((comment) => {
-      // parentElement = node.parentElement;
-
       const commentUl = document.getElementById(`${this.id}commentUl`);
       const commentLi = document.createElement("LI");
       commentLi.innerText = comment.content;
       commentUl.appendChild(commentLi);
     });
-    // debugger;
-
-    // if (x.style.display === "none") {
-    //   // add state to display
-    //   x.innerHTML = `Try out ${name} at ${city}!`;
-    //   x.style.display = "block";
-    // } else {
-    //   x.style.display = "none";
-    // }
   }
 
   save() {
     app.courses.push(this);
-    // app.renderCourses();
-    // debugger;
     this.display();
   }
 }
